76f7178527b2b7cb9bf6f8ee4b6cc08d324b99b2
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2022-09-15 17:38:08 -0400

    max-age added for browser caching of the server response

--- src/details.js
+++ src/details.js
@@ -1,79 +1,32 @@
 import puppeteer from 'puppeteer';
 
 const memoizedFileExtensions = {};
-// const dayInMilliseconds = 86400000;
-const dayInMilliseconds = 30000; // testing using 30 seconds
+const secondsBeforeRefresh = 2592000; // default is seconds for 30 days, this is the default max age given from fileinfo on one of the file extension images
+// const secondsBeforeRefresh = 30; // testing
 
 const details = async (req, res) => {
+	// console.log('details requested');
 	const { extension } = req.query;
 
 	if (memoizedFileExtensions[extension]) {
 		const { json, lastRetrieved } = memoizedFileExtensions[extension];
 
 		// refresh asynchronously for the next time, immediately return the current value to the requester
-		if (lastRetrieved + dayInMilliseconds < Date.now()) {
-			refreshMemo(extension);
+		if (lastRetrieved + secondsBeforeRefresh * 1000 < Date.now()) {
+			puppeteerLaunch({ extension });
 		}
 
 		res.status(200).json(json);
 
 		return;
 	}
-	puppeteer
-		.launch({
-			headless: true,
-		})
-		.then(async (browser) => {
-			const page = await browser.newPage();
 
-			let url = `https://fileinfo.com/extension/${extension}`;
-			await page.goto(url, {
-				waitUntil: 'domcontentloaded',
-				timeout: 0,
-			});
-
-			const extIconArr = await page.$$eval('.entryIcon', (imgs) => {
-				return imgs.map((img) => ({
-					normal: img.getAttribute('data-bg'),
-					large: img.getAttribute('data-bg-lg'),
-				}));
-			});
-			const extFullNameArr = await page.$$eval('.title', (titles) => {
-				return titles.map((title) => title.innerHTML);
-			});
-
-			if (extIconArr.length == 0 || extFullNameArr.length == 0) {
-				const json = { message: 'File extension could not be found' };
-				memoizedFileExtensions[extension] = {
-					json,
-					lastRetrieved: Date.now(),
-				};
-				res.status(400).json(json);
-			} else {
-				const json = {
-					icons: extIconArr[0],
-					fullName: extFullNameArr[0],
-				};
-				memoizedFileExtensions[extension] = {
-					json,
-					lastRetrieved: Date.now(),
-				};
-				res.status(200).json(json);
-			}
-		});
+	puppeteerLaunch({ extension, res });
 };
 
 export default details;
 
-// refreshes in case urls change
-const refreshMemo = async (extension) => {
-	// update teh lastRetrieved here, so it doesnt wait for the browser page to load entirely, and allow multiple refreshMemo calls unnecessarily
-	memoizedFileExtensions[extension] = {
-		...memoizedFileExtensions[extension], // spreads to not lose the json property, until the new json property is retrieved
-		lastRetrieved: Date.now(),
-	};
-
-	// console.log('refreshingggggggggggggggggggg');
+const puppeteerLaunch = ({ extension, res }) => {
 	puppeteer
 		.launch({
 			headless: true,
@@ -99,11 +52,9 @@ const refreshMemo = async (extension) => {
 
 			if (extIconArr.length == 0 || extFullNameArr.length == 0) {
 				const json = { message: 'File extension could not be found' };
-				memoizedFileExtensions[extension] = {
-					json,
-					lastRetrieved: Date.now(),
-				};
-				// res.status(400).json(json);
+				if (res) {
+					res.status(400).json(json);
+				}
 			} else {
 				const json = {
 					icons: extIconArr[0],
@@ -113,7 +64,15 @@ const refreshMemo = async (extension) => {
 					json,
 					lastRetrieved: Date.now(),
 				};
-				// res.status(200).json(json);
+				// console.log('test');
+				if (res) {
+					res
+						.status(200)
+						.set({
+							'cache-control': `max-age=${secondsBeforeRefresh}`, // browser caches the response
+						})
+						.json(json);
+				}
 			}
 		});
 };
