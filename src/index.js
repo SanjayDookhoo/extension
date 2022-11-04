3910657e2fc9be2bcb7ca790e1e097b5624453ea
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2022-11-04 00:00:51 -0400

    other docker changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import details from './details.js';
 import puppeteer from 'puppeteer';
 
 export const browser = await puppeteer.launch({
-	// executablePath: '/usr/bin/google-chrome', // https://dev.to/cloudx/how-to-use-puppeteer-inside-a-docker-container-568c
+	executablePath: '/usr/bin/google-chrome', // https://dev.to/cloudx/how-to-use-puppeteer-inside-a-docker-container-568c
 	headless: true,
 	args: ['--no-sandbox'],
 	timeout: 0,
